7f9832fa9cbf8b212cdbbe69ac08aa416509e2e1
Ulises Cappa <ulisescappa@gmail.com>
2023-09-26 17:46:54 -0300

    Agregando estilos simples a la estructura basica. Modal agregado

---
+++ scripts/perfil.js
@@ -0,0 +1,6 @@
+const cambiarPassBtn = document.querySelector('.cambiar-pass-btn');
+const dialog = document.querySelector('.change-pass-modal');
+cambiarPassBtn.addEventListener('click', () => {
+    dialog.showModal();
+}
+);
